8983304f49f4089d185e83e06a7901082e13fe20
Bluebee45 <santi72143@gmail.com>
2023-08-17 22:50:48 -0500

    add file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,47 +1,51 @@
 #!/usr/bin/python3
-"""Import Modules"""
+"""
+Script that starts a Flask web application
+"""
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_route():
-    """Main Route"""
-    return 'Hello HBNB!'
+def index():
+    """ Returns 'Hello HBNB!' """
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """hbnb Route"""
-    return 'HBNB'
+    """ Returns 'HBNB' """
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_route(text):
-    """c route , return text with spaces"""
-    text_with_spaces = text.replace('_', ' ')
-    return f"C {text_with_spaces}"
+def c_is_fun(text):
+    """ Returns 'C ' followed by the value of the text variable """
+    return "C {}".format(text.replace("_", " "))
 
 
-@app.route('/python/', defaults={'text': 'is_cool'})
+@app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text):
-    """python route, return text with spaces"""
-    return "Python {}".format(text.replace('_', ' '))
+def python_route(text="is cool"):
+    """
+    Display “Python ”, followed by the value of the text variable
+    The default value of text is: "is cool"
+    """
+    return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
-def number_route(n):
-    """number route, return if only is an integer"""
-    return f"{n} is a number"
+def number(n):
+    """ Returns 'n is a number' only if n is an int """
+    return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """Number route, display a HTML page"""
-    return render_template('5-number.html', name=n)
+    """ Returns a HTML page only if n is an int """
+    return render_template("5-number.html", n=n)
 
 
-"""Entry Point"""
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
