3dd9469b889bf03c6b733743e190901ef053c285
Bluebee45 <santi72143@gmail.com>
2023-08-18 22:12:50 -0500

    add file

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Script that starts a Flask web application
+starts a Flask web app
 """
 from flask import Flask, render_template
 
@@ -9,41 +9,40 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
-    """ Returns 'Hello HBNB!' """
+    """returns hello HBNB!"""
     return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """ Returns 'HBNB' """
+    """returns HBNB"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_is_fun(text):
-    """ Returns 'C ' followed by the value of the text variable """
+    """display “C ” followed by the value of the text variable"""
     return "C {}".format(text.replace("_", " "))
 
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def python_route(text="is cool"):
-    """
-    Display “Python ”, followed by the value of the text variable
-    The default value of text is: "is cool"
+def python_is_cool(text="is cool"):
+    """display “Python ”, followed by the value of the text variable
+       default value of text: is cool
     """
     return "Python {}".format(text.replace("_", " "))
 
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
-    """ Returns 'n is a number' only if n is an int """
+    """display "n is a number" only if n is an int"""
     return "{} is a number".format(n)
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
-    """ Returns a HTML page only if n is an int """
+    """display a HTML page only if n is an int"""
     return render_template("5-number.html", n=n)
 
 
